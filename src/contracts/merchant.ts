54b1826e3a9856985ea3025e4fa4d5adf96c6046
Mighty Ali <Mi3liX9@Outlook.sa>
2025-05-28 10:27:21 +0300

    add owner merchants

--- src/contracts/merchant.ts
+++ src/contracts/merchant.ts
@@ -4,15 +4,22 @@ import z from "zod/v4";
 const createMerchant = oc
   .input(
     z.object({
-      name: z.string(),
-      description: z.string(),
-      slug: z.string(),
-      logoUrl: z.string().optional(),
-      bannerUrl: z.string().optional(),
-      businessEmail: z.email(),
-      phoneNumber: z.string(),
-      country: z.string().default("SA"),
-      status: z.enum(["ACTIVE", "INACTIVE"]).default("INACTIVE"),
+      // owner
+      ownerName: z.string(),
+      ownerAddress: z.string(),
+      ownerEmail: z.email(),
+      ownerPhone: z.string(),
+      ownerIdNumber: z.int().transform((value) => value.toString()),
+      // merchant
+      merchantName: z.string(),
+      merchantDescription: z.string(),
+      merchantSlug: z.string(),
+      merchantLogoUrl: z.string().optional(),
+      merchantBannerUrl: z.string().optional(),
+      merchantBusinessEmail: z.email(),
+      merchantPhoneNumber: z.string(),
+      merchantCountry: z.string().default("SA"),
+      merchantStatus: z.enum(["ACTIVE", "INACTIVE"]).default("INACTIVE"),
     })
   )
   .output(z.object({ id: z.string() }));
